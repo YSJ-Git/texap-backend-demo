f42c048fd476731dee17cb2a2b4a0e447f7e9573
YSJ-Git <100123808+YSJ-Git@users.noreply.github.com>
2022-03-06 03:22:54 +0900

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,21 +1,65 @@
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
+const parse = require('pg-connection-string').parse;
+
+module.exports = ({ env }) => {
+
+  if(env('NODE_ENV') === 'production'){
+    const config = parse(process.env.DATABASE_URL)
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default:{
+          connector:'bookshelf',
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          username: config.user,
+          password: config.password
+        },
+        options: {
+          ssl: false
+        }
+      }
+    }
+  }
+
+  return {
+      defaultConnection: "default",
   connections: {
     default: {
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: env("DATABASE_HOST", "postgres://ahbhenollewmqy:e6a9c3fbddd00333bb9452d62ceb7521869ec973a34f283d4399a069a91a7390@ec2-44-194-167-63.compute-1.amazonaws.com:5432/d7u6gegnmbet3d"),
-        //port: env.int("DATABASE_PORT", 5433),
-        //database: env("DATABASE_NAME", "texap-api"),
-        //username: env("DATABASE_USERNAME", "postgres"),
-        //password: env("DATABASE_PASSWORD", "dbst4862"),
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5433),
+        database: env("DATABASE_NAME", "texap-api"),
+        username: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "dbst4862"),
         ssl: env.bool("DATABASE_SSL", false),
       },
       options: {},
     },
   },
-});
+  }
+};
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: "default",
+//   connections: {
+//     default: {
+//       connector: "bookshelf",
+//       settings: {
+//         client: "postgres",
+//         host: env("DATABASE_HOST", "127.0.0.1"),
+//         port: env.int("DATABASE_PORT", 5433),
+//         database: env("DATABASE_NAME", "texap-api"),
+//         username: env("DATABASE_USERNAME", "postgres"),
+//         password: env("DATABASE_PASSWORD", "dbst4862"),
+//         ssl: env.bool("DATABASE_SSL", false),
+//       },
+//       options: {},
+//     },
+//   },
+// });
 
 // module.exports = ({ env }) => ({
 //   defaultConnection: "default",
